3b5f5a2aa14bf1bb0f4ba3b43e154f6310a4fdba
Silvia Brown <siltastic@gmail.com>
2015-08-18 18:15:32 -0400

    Added the angle between the hands function.

--- sundial.js
+++ sundial.js
@@ -3,7 +3,17 @@ module.exports = {
     return (minute * 6);
   },
 
-  hour: function(hour){
-    return (hour * 15);
+  hour: function(hour, minute){
+    return (hour * 30) + (minute * 0.5);
+  },
+
+  angleBetween: function(hour, minute){ 
+    hourAngle = this.hour(hour, minute) 
+    minuteAngle = this.minute(minute)
+
+    // this.minute is invoking the minute: function above and returns the minute angle into the minuteAngle variable.
+    // this.hour is invoking the hour: function above and returns the hour angle into the hourAngle variable.
+    var angle = Math.abs( hourAngle - minuteAngle );
+    return Math.min(angle, 360 - angle);
   }
 };
