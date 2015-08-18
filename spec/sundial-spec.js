7ff54762562010daa7995b842a51af4c3f579f6e
Omari Mayers-Walker <omari@fastmail.fm>
2015-08-17 21:28:58 -0400

    added function that checks time input is formated correctly

--- spec/sundial-spec.js
+++ spec/sundial-spec.js
@@ -2,8 +2,13 @@
 
 var sundial = require("../sundial")
 
-describe("hour", function(){
-    it("should be a number 1 though 12", function(){
-        // var hour = sundial.
+describe("hours <= 12 && minutes <= 59", function(){
+    it("should be a number less than 13", function(){
+        var hour = sundial.hourMinHands(12)
+        expect(hour).toBeLessThan(13)
+    })
+    it("should be a number less than 60", function(){
+        var minute = sundial.hourMinHands(59)
+        expect(minute).toBeLessThan(60)
     })
 })
--- sundial.js
+++ sundial.js
@@ -1,4 +1,12 @@
 //sundial.js
 
 module.exports = {
+    hourMinHands: function(hour, minute) {
+        var time = []
+        if (hour <= 12 && minute <= 60) {
+            time.push(hour)
+            time.push(minute)
+        }
+        return time
+    }
 }
