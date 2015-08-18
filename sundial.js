8f3f82d04827deeeb489cbde88838f4b72ccabb7
Paul Doulatshahi <paul.doulatshahi@gmail.com>
2015-08-18 09:54:12 -0400

    changed if statements to ternary and math.min

--- sundial.js
+++ sundial.js
@@ -1,21 +1,8 @@
 exports.sundial = function(hour, minute) {
-  // declare hourpos and minutepos variables
-  var hourpos;
-  var minutepos;
-  // calculate hour hand position
-  // if hour = 12 we need different math
-  if (hour == 12){
-    var hourpos = minute / 2
-  }
-  // hour and minutes affect position of hour hand
-  else {
-    var hourpos = hour * 30 + minute / 2
-  }
-  var minutepos = minute * 6;
-  if (hourpos - minutepos > 180){
-    return 360 - (hourpos - minutepos)
-    }
-  else {
-    return Math.abs(hourpos - minutepos)
-  }
+  //use ternary (if statements are so week 2) to calculate hrangle bc if hour = 12 then we need different math
+  var hrangle = hour == 12 ? minute / 2 : hour * 30 + minute / 2;
+  // calculate minangle 360 degrees, 60 minutes so multiply by 6 to get angle
+  var minangle = minute * 6;
+  // calculate absolute value difference between angles - find smaller angle difference in circle (should always be 180 or less)
+  return Math.min(360 - Math.abs(hrangle - minangle), Math.abs(hrangle - minangle));
 }
