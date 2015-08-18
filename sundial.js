2eba860a52888229f32c38847ed2bc0a562dcb7e
Robert <i.am.robert.benjamin@gmail.com>
2015-08-17 22:18:26 -0400

    Refactor code
    Removed some unnecessary else statements and cleaned up the final
    return.

--- sundial.js
+++ sundial.js
@@ -2,8 +2,6 @@ sundial = {
 	hours: function(hour) {
 		if (hour > 12) {
 			return "AM and PM, baby, use them.";
-		} else if (hour == 12) {
-			return 0;
 		} else {
 			return hour * 30;
 		}
@@ -11,19 +9,13 @@ sundial = {
 	minutes: function(minute) {
 		if (minute > 60) {
 			return "Can't be greater than 60 minutes!"
-		} else if (minute == 60) {
-			return 0;
 		} else {
 			return minute * 6;
 		}
 	},
 	difference: function(hour, minute) {
 		var difference = Math.abs(this.minutes(minute) - this.hours(hour))
-		if (difference > 180) {
-			return 360 - difference
-		} else {
-			return difference
-		}
+		return Math.min(difference, 360 - difference)
 	}
 }
 
