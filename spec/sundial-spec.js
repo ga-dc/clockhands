f3151f09d15dfccbeca5893c4947d1fa99172730
Silvia Brown <siltastic@gmail.com>
2015-08-18 18:17:30 -0400

    Added angle between hands.

--- spec/sundial-spec.js
+++ spec/sundial-spec.js
@@ -1,24 +1,28 @@
 // Reference the file to test.
 var sundial = require( "../sundial" )
 
-// Use 5:30PM as an example.
-// Hour * 15 = Hour Angle (PM) ... I think?
-// Hour * 30 = Hour Angle (AM) ... I think?
+// Hour * 30 = Hour Angle ... I think?
 // Minute * 6 = Minute Angle
 
 // Add minute function.
-describe("What is the minute angle?", function(){
-  it("Returns the minute angle", function(){
-    var minute = sundial.minute(30);
-    expect(minute).toBe(180);
+describe("handMinute", function(){
+  it("Returns the minute angle.", function(){
+    var minute = sundial.minute(2);
+    expect(minute).toBe(12);
   });
 });
 
 // Add hour function.
-describe("What is the hour angle?", function(){
+describe("handHour", function(){
   it("Return the hour angle.", function(){
-    var hour = sundial.hour(5); //5:00PM
-    expect(hour).toBe(75);
+    var hour = sundial.hour(3, 0);
+    expect(hour).toBe(90);
   });
 });
 
+describe("angleBetweenHands", function(){
+  it ("Returns the smaller angle between the two hands.", function(){
+    var angle = sundial.angleBetween(12, 0)
+    expect(angle).toBe(0)
+  });
+});
