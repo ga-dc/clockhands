c054b6b39dcca4ed8e2e07df99c061da7ac4ed02
Paul Doulatshahi <paul.doulatshahi@gmail.com>
2015-08-18 10:03:30 -0400

    added variable for anglediff to clean code

--- sundial.js
+++ sundial.js
@@ -1,8 +1,10 @@
 exports.sundial = function(hour, minute) {
-  //use ternary (if statements are so week 2) to calculate hrangle bc if hour = 12 then we need different math
+  // hour = 12 should be considered as hr = 0 so use ternary
   var hrangle = hour == 12 ? minute / 2 : hour * 30 + minute / 2;
   // calculate minangle 360 degrees, 60 minutes so multiply by 6 to get angle
   var minangle = minute * 6;
-  // calculate absolute value difference between angles - find smaller angle difference in circle (should always be 180 or less)
-  return Math.min(360 - Math.abs(hrangle - minangle), Math.abs(hrangle - minangle));
+  // calculate absolute value difference between angles
+  var anglediff = Math.abs(hrangle - minangle);
+  // find smaller angle difference depending on direction around circle
+  return Math.min(360 - anglediff, anglediff);
 }
