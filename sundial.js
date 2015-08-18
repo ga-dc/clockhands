09b8843643a177f9a03921807cd22c8f2434a472
Robert <i.am.robert.benjamin@gmail.com>
2015-08-17 22:24:07 -0400

    Remove checking user input
    What kind of user doesn’t know that minutes max out at 60?

--- sundial.js
+++ sundial.js
@@ -1,21 +1,9 @@
 sundial = {
-	hours: function(hour) {
-		if (hour > 12) {
-			return "AM and PM, baby, assume we use them.";
-		} else {
-			return hour * 30;
-		}
-	},
-	minutes: function(minute) {
-		if (minute > 60) {
-			return "Can't be greater than 60 minutes!"
-		} else {
-			return minute * 6;
-		}
-	},
+	hours: function(hour) { return hour * 30; },
+	minutes: function(minute) { return minute * 6; },
 	difference: function(hour, minute) {
-		var difference = Math.abs(this.minutes(minute) - this.hours(hour))
-		return Math.min(difference, 360 - difference)
+		var diff = Math.abs(this.minutes(minute) - this.hours(hour))
+		return Math.min(diff, 360 - diff)
 	}
 }
 
