fe96a550b38468daa77187b63117b931138c0071
Robert <i.am.robert.benjamin@gmail.com>
2015-08-18 21:58:39 -0400

    Fix incorrect hour hand
    Wasn’t accounting for the hour hand partially turning, was assuming
    hour hand jumped between hours (which isn’t how clocks **usually**
    work).

--- spec/sundial-spec.js
+++ spec/sundial-spec.js
@@ -2,11 +2,11 @@ var sundial = require("../sundial")
 
 describe("hours", function() {
 	it("should calculate degrees with just the hour hand at 6", function() {
-		var degrees = sundial.hours(6);
+		var degrees = sundial.hours(6, 0);
 		expect(degrees).toBe(180);
 	})
 	it("should calculate degrees with just the hour hand at 3", function() {
-		var degrees = sundial.hours(3);
+		var degrees = sundial.hours(3, 0);
 		expect(degrees).toBe(90);
 	})
 })
@@ -25,14 +25,14 @@ describe("minutes", function() {
 describe("difference", function() {
 	it("should return difference in degrees when the hour is 6 and the minutes are 30", function() {
 		var degrees = sundial.difference(6, 30);
-		expect(degrees).toBe(0);
+		expect(degrees).toBe(15);
 	})
 	it("should return difference in degrees when the hour is 9 and the minutes are 0", function() {
 		var degrees = sundial.difference(9, 0);
 		expect(degrees).toBe(90);
 	})
-	it("should return difference in degrees when the hour is 1 and the minutes are 0", function() {
-		var degrees = sundial.difference(1, 0);
-		expect(degrees).toBe(30);
+	it("should return difference in degrees when the hour is 2 and the minutes are 45", function() {
+		var degrees = sundial.difference(2, 45);
+		expect(degrees).toBe(172.5);
 	})
 })
--- sundial.js
+++ sundial.js
@@ -1,8 +1,8 @@
 sundial = {
-	hours: function(hour) { return hour * 30; },
+	hours: function(hour, minute) { return (hour * 60 + minute) / 2; },
 	minutes: function(minute) { return minute * 6; },
 	difference: function(hour, minute) {
-		var diff = Math.abs(this.minutes(minute) - this.hours(hour))
+		var diff = Math.abs(this.minutes(minute) - this.hours(hour, minute))
 		return Math.min(diff, 360 - diff)
 	}
 }
