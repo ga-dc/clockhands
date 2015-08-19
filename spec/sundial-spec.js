42786ec815b0ef9ad8e9178c2dfcf9153d6dd233
Omari Mayers-Walker <omari@fastmail.fm>
2015-08-19 00:00:25 -0400

    completed

--- spec/sundial-spec.js
+++ spec/sundial-spec.js
@@ -2,13 +2,29 @@
 
 var sundial = require("../sundial")
 
-describe("hours <= 12 && minutes <= 59", function(){
-    it("should be a number less than 13", function(){
+describe("hours <= 12 && minutes <= 59", function() {
+    it("should be a number less than 13", function() {
         var hour = sundial.hourMinHands(12)
         expect(hour).toBeLessThan(13)
     })
-    it("should be a number less than 60", function(){
+    it("should be a number less than 60", function() {
         var minute = sundial.hourMinHands(59)
         expect(minute).toBeLessThan(60)
     })
+
+    it("should be 0 for 12 noon", function() {
+        var degree = sundial.hands(12, 0);
+        expect(degree).toBe(0);
+    })
+
+    it("should be 180 for 6", function() {
+        var degree = sundial.hands(6, 0);
+        expect(degree).toBe(180);
+    })
+
+    it("should be at 90 at 2:30", function() {
+        var degree = sundial.hands(2, 30);
+        expect(degree).toBe(105);
+    })
+
 })
--- sundial.js
+++ sundial.js
@@ -1,6 +1,7 @@
 //sundial.js
 
 module.exports = {
+
     hourMinHands: function(hour, minute) {
         var time = []
         if (hour <= 12 && minute <= 60) {
@@ -8,5 +9,13 @@ module.exports = {
             time.push(minute)
         }
         return time
+    },
+
+    hands: function(hours, minutes) {
+        var hourHand = 0.5 * (hours * 60 + minutes)
+        var minuteHand = 6 * minutes
+        var hands = Math.abs(hourHand - minuteHand)
+        hands = Math.min(hands, 360 - hands)
+        return hands
     }
 }
