aa3e7da3ac61435892c41e055da2d4eb7272668e
Laura Nawrocki <lwn2@aol.com>
2015-08-21 00:48:29 -0400

    2 tests working

---
+++ sundial.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+  hourAngle: function (hours) {
+    var hourAngle = hours * 30;
+    return hourAngle;
+  },
+  minuteAngle: function (minutes){
+    var minuteAngle = minutes * 6;
+    return minuteAngle;
+  },
+}
