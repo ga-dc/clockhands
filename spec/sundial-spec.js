340f30e19ad7196c19b5594b866e990250fb89ff
ktw1222 <ktw1222@gmail.com>
2015-08-21 00:54:17 -0400

    sundial hw

--- spec/sundial-spec.js
+++ spec/sundial-spec.js
@@ -1,1 +1,26 @@
 var sundial = require("../sundial");
+
+describe("a sundial's", function(){
+
+  describe("hourHand", function(){
+    it("should return the angle of hour hand by degree", function(){
+      var hourAngle = sundial.hourHandAngle(9,0);
+      expect(hourAngle).toBe(90);
+    })
+  })
+
+  describe("minuteHand", function(){
+    it("should return the angle of minute hand by degree", function(){
+      var minuteAngle = sundial.minuteHandAngle(10);
+      expect(minuteAngle).toBe(60);
+    })
+  })
+
+  describe("angleBetweenHands", function(){
+    it("should return the smaller angle between hourHand and minuteHand", function(){
+      var angle = sundial.betweenAngle(4,0);
+      expect(angle).toBe(120);
+    })
+  })
+
+})
--- sundial.js
+++ sundial.js
@@ -1,13 +1,47 @@
 module.exports = {
-  
+  betweenAngle: function(hour, minute){
+    var angle = Math.abs(hourHandAngle(hour, minute) - minuteHandAngle(minute));
+    return Math.min(angle, 360 - angle);
+  },
+
+  hourHandAngle: function(hour, minute){
+    return (60 * hour + minute ) / 2 ;
+  },
+
+  minuteHandAngle: function(minute){
+    return minute * 6;
+  }
+}
+
+// put these functions in module.exports
+
+// function hourAngle(hour, minute){
+//   return (hour * 30) - (minute * 6);
+// }
+//
+// function minuteAngle(minute){
+//   return minute * 6;
+// }
+//
+// function betweenAngle(hour, minute){
+//   var angle = Math.abs(hourHandAngle(hours, minutes) - minuteHandAngle(minutes));
+//   return Math.min(angle, 360 - angle);
+// }
 
 
 
 
-
-
-
-
-
-
-}//
+// function angleBetweenHands(time){
+//   var hours = parseInt(time.split(':')[0]);
+//   var minutes = parseInt(time.split(':')[1]);
+//   var angle = Math.abs(hourHandAngle(hours, minutes) - minuteHandAngle(minutes));
+//   return Math.min(angle, 360 - angle);
+// }
+//
+// function hourHandAngle(hours, minutes){
+//   return (60 * hours + minutes) / 2
+// }
+//
+// function minuteHandAngle(minutes){
+//   return 6 * minutes
+// }
